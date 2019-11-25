70ac3e24f6f92a119f38987982deea80c8cdfadb
Evan Smith <smith.m.evan@gmail.com>
2019-11-25 14:46:43 +0000

    finished 4 tests

--- Part A/specs/student_spec.rb
+++ Part A/specs/student_spec.rb
@@ -11,16 +11,23 @@ def setup
 end
 
 def test_get_student_name
-  assert_equal("Evan", @test_student1.get_student_name)
+  assert_equal("Evan", @test_student1.name)
 end
 
 def test_get_student_cohort
-  assert_equal("E36", @test_student1.get_student_cohort)
+  assert_equal("E36", @test_student1.cohort)
 end
 
 def test_change_student_name
+  @test_student1.change_student_name("Eva")
+  assert_equal("Eva", @test_student1.name)
+end
 
-end 
+def test_change_student_cohort
+  @test_student1.change_student_cohort("E37")
+  assert_equal("E37", @test_student1.cohort)
+end
+
 
 
 end
--- Part A/student.rb
+++ Part A/student.rb
@@ -1,16 +1,26 @@
 class Student
 
+attr_accessor :name, :cohort
+
   def initialize(name, cohort)
     @name = name
     @cohort = cohort
   end
 
-  def get_student_name()
-    return @name
+  # def get_student_name()
+  #   return @name
+  # end
+  #
+  # def get_student_cohort()
+  #   return @cohort
+  # end
+
+  def change_student_name(new_name)
+    @name = new_name
   end
 
-  def get_student_cohort()
-    return @cohort
+  def change_student_cohort(new_cohort)
+    @cohort = new_cohort
   end 
 
 end
