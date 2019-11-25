9f021a8d1a6503b53995fd4cec4b3cff828455d9
Evan Smith <smith.m.evan@gmail.com>
2019-11-25 21:40:44 +0000

    got extension to pass one test for adding a book

--- Extension/library.rb
+++ Extension/library.rb
@@ -1,17 +1,26 @@
 class Book
+
   def initialize(title)
     @title = title
   end
+
 end
-class Library
-  def initialize
-    @books = []
+
+  class Library
+    def initialize
+      @books = []
+    end
+
+    # def get_book
+    #   return @books
+    # end
+    def get_book(search_name)
+      for book in @books
+        return search_name if book == search_name
+      end
+    end
+
+    def add_book(book)
+      @books.push(book)
+    end
   end
-  def add_book(book)
-    @books.push(book)
-  end
-end
-# In our tests
-book = Book.new("War and Peace")
-library = Library.new()
-library.add_book(book)
--- Extension/specs/library_specs.rb
+++ Extension/specs/library_specs.rb
@@ -6,4 +6,32 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 class TestLibrary < MiniTest::Test
 
-end 
+  def setup
+    book = Book.new({title: "Lord of the Rings",
+      rental_details: {
+      student_name: "Jeff", date: "01/12/16"
+    }
+      })
+
+  end
+
+  # def test_get_book()
+  #   book = {title: "Lord of the Rings",
+  #     rental_details: {
+  #     student_name: "Jeff", date: "01/12/16"
+  #   }
+  #   assert_equal(library.get_book(book), library.get_name(book))
+  # end
+
+  def test_add_book
+    book = Book.new(title: "War and Peace",
+    rental_details: {
+      student_name: "George", date: "03/09/18"
+      })
+      library = Library.new()
+    assert_equal(library.get_book(book), library.add_book(book))
+  end
+
+
+
+end
